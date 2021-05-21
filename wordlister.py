15ec0f04b2ee1daac215d48104772d4908c3d257
Aaron Thomas <84493696+BLINKING-IDIOT@users.noreply.github.com>
2021-05-21 11:02:49 +0000

    create a wordlist using python

---
+++ wordlister.py
@@ -0,0 +1,55 @@
+import itertools
+import os 
+os.system('clear')
+r = "\033[31m"
+g = "\033[32m"
+y = "\033[33m"
+logo =f"""{y}
+           [wordlister]{r}
+       █████████████████████
+    ████▀                 ▀████
+  ███▀                       ▀███
+ ██▀                           ▀██
+█▀                               ▀█
+█                                 █
+█                                 █
+█                                 █
+█   █████                 █████   █
+█  ██▓▓▓███             ███▓▓▓██  █
+█  ██▓▓▓▓▓██           ██▓▓▓▓▓██  █
+█  ██▓▓▓▓▓▓██         ██▓▓▓▓▓▓██  █
+█▄  ████▓▓▓▓██       ██▓▓▓▓████  ▄█
+▀█▄   ▀███▓▓▓██     ██▓▓▓███▀   ▄█▀
+  █▄    ▀█████▀     ▀█████▀    ▄█
+ ▄██           ▄█ █▄           ██▄
+ ███           ██ ██           ███
+ ███                           ███
+  ▀██  ██▀██  █  █  █  ██▀██  ██▀
+   ▀████▀ ██  █  █  █  ██ ▀████▀
+    ▀██▀  ██  █  █  █  ██  ▀██▀
+          ██  █  █  █  ██
+          ██  █  █  █  ██
+          ██  █  █  █  ██
+          ██  █  █  █  ██
+          ██  █  █  █  ██
+          ██  █  █  █  ██
+          ██  █  █  █  ██
+          ██  █  █  █  ██
+          ██  █  █  █  ██
+          ██  █  █  █  ██
+          ██  █  █  █  ██
+          ██  █  █  █  ██
+           █▄▄█▄▄█▄▄█▄▄█
+           
+         {g}by BLINKING-IDIOT
+      insta: @a.r.n.beatzzzz
+"""
+print(logo)
+k = 'abcdefghijklmnopqrstuvwxyz'
+y=input("Do you what to add numbers? (y/n)")
+if y=='y':
+	k+='1234567890'
+le = int(input("Enter the length of words using:"))
+for i in itertools.product(k, repeat=le):
+ x=(''.join(i))
+ print (x)
